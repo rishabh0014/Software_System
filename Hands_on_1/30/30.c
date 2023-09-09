f3b6cc95bfd50d573fdb074d672bc2d06f454211
rishabh0014 <rishabhteli14@gmail.com>
2023-09-09 21:42:57 +0530

    Final_Commit

--- Hands_on_1/30/30.c
+++ Hands_on_1/30/30.c
@@ -15,26 +15,15 @@ Description : 30. Write a program to run a script at a specific time using a Dae
 int main(void)
 {
 
-    struct tm exectime;
-    exectime.tm_hour = 1;
-	exectime.tm_min = 25;
+    FILE*fp = fopen("x.txt","w");
 	if(fork()==0){
 		setsid();
 		umask(0);
 		chdir("/");
 		while(1){
-		    time_t timevalue = time(NULL);
-		    struct tm* currtime = localtime(&timevalue);
-
-		    if(currtime->tm_hour == exectime.tm_hour && currtime->tm_min == exectime.tm_min){
-
-                int result = system("/home/rishabh/Desktop/IIITB/Software_System/Software_System/Hands_on_1/30/temp.sh");
-                if (result == -1) {
-                    perror("system");
-                }
-                return 0;
-                break;
-		    }
+		    sleep(5);
+			fprintf(fp,"Hello\n");
+			fflush(fp);
 	    }
 	}else{
 		exit(0);
